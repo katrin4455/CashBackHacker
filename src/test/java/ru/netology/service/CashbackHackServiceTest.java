47ee08b9e5223a636ffaf4fa0302844f6fedd770
katrin4455 <katrin.tem2013@gmail.com>
2022-11-14 17:23:52 +0300

    add testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void boundary() {
+        int amount = 1000;
+        int expected = 1000;
+        int actual = service.remain(amount);
+
+        assertEquals(expected,actual);
+
+    }
+
+   @Test
+    public void boundaryUnder() {
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        assertEquals(expected,actual);
+   }
+   @Test
+    public void boundaryOver() {
+       int amount = 0;
+       int expected = 1000;
+       int actual = service.remain(amount);
+
+       assertEquals(expected,actual);
+   }
+}
